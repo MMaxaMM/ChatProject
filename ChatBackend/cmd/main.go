cf331904546d8f46f0c8ac5f52e974e6ec4502e3
Maxim Butakof <maxim.butakof@gmail.com>
2024-10-18 05:19:22 +0300

    Новая логика main

--- ChatBackend/cmd/main.go
+++ ChatBackend/cmd/main.go
@@ -1,19 +1,83 @@
 package main
 
 import (
-	"chat/internal/http-server/handlers"
+	"chat/internal/config"
+	"chat/internal/repository"
 	"log"
-	"net/http"
+	"os"
 
-	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/chat", handlers.Chat).Methods("POST")
-	router.HandleFunc("/history", handlers.History).Methods("POST")
+	// Загрузка .env файла, расположенного локально
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
 
-	log.Println("Run LLM Server")
-	err := http.ListenAndServe(":5050", router)
-	log.Fatal(err)
+	// Загрузка конфигураций
+	cfg := config.MustLoad()
+
+	// Инициализация базы данных
+	db, err := repository.NewPostgresDB(&repository.Config{
+		Host:     cfg.Host,
+		Port:     cfg.Port,
+		Username: cfg.Username,
+		DBName:   cfg.DBName,
+		SSLMode:  cfg.SSLMode,
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	_ = repository.NewPostgresRepository(db)
+
+	// Тесты, надо завернуть в тест
+
+	// x, err := rep.GetUser("admin", "admin")
+	// log.Println(x)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	z, ok := err.(*pq.Error)
+	// 	if ok {
+	// 		log.Println(z.Code)
+	// 	}
+	// }
+
+	// err = rep.SaveChatItem(&chat.ChatItem{
+	// 	UserId: 1,
+	// 	ChatId: 1,
+	// 	Message: chat.Message{
+	// 		Role:    chat.RoleUser,
+	// 		Content: "3",
+	// 	},
+	// })
+	// if err != nil {
+	// 	log.Println(err)
+	// 	z, ok := err.(*pq.Error)
+	// 	if ok {
+	// 		log.Println(z.Code)
+	// 	}
+	// }
+
+	// x, err := rep.GetHistory(&chat.HistoryRequest{UserId: 1, ChatId: 1}, repository.NoLimit)
+	// log.Println(x)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	z, ok := err.(*pq.Error)
+	// 	if ok {
+	// 		log.Println(z.Code)
+	// 	}
+	// }
+
+	// err = rep.DeleteChat(&chat.HistoryRequest{UserId: 1, ChatId: 1})
+	// if err != nil {
+	// 	log.Println(err)
+	// 	z, ok := err.(*pq.Error)
+	// 	if ok {
+	// 		log.Println(z.Code)
+	// 	}
+	// }
 }
