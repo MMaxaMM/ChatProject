f75181d9213ac8e5880d8f807714d9e8ce5e977a
Maxim Butakof <maxim.butakof@gmail.com>
2024-10-19 04:33:16 +0300

    Обновлён api

--- ChatBackend/internal/api/llmapi/llmapi.go
+++ ChatBackend/internal/api/llmapi/llmapi.go
@@ -1,39 +1,29 @@
-package api
+package llmapi
 
 import (
 	"bytes"
+	"chat"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
 
-type MessageRole string
-
-const (
-	RoleSystem    = "system"
-	RoleUser      = "user"
-	RoleAssistent = "assistent"
-)
-
-// JSON ответа от LLM
-type Message struct {
-	Role    MessageRole `json:"role"`
-	Content string      `json:"content"`
-}
-
 // JSON запроса к LLM
-type ChatRequest struct {
-	Messages  []Message `json:"messages"`
-	MaxTokens uint      `json:"max_tokens"`
+type Request struct {
+	Messages  []chat.Message `json:"messages"`
+	MaxTokens uint           `json:"max_tokens"`
 }
 
 type Client struct {
-	URL       string
-	MaxTokens uint
+	URL string
 }
 
-func (client *Client) Generate(req *ChatRequest) (*Message, error) {
+func NewClient(url string) *Client {
+	return &Client{URL: url}
+}
+
+func (client *Client) Generate(req *Request) (*chat.Message, error) {
 	jsonRequest, err := json.Marshal(req)
 	if err != nil {
 		return nil, fmt.Errorf("failed to serialize request: %w", err)
@@ -50,15 +40,11 @@ func (client *Client) Generate(req *ChatRequest) (*Message, error) {
 		return nil, fmt.Errorf("failed to read response body: %w", err)
 	}
 
-	var message *Message
+	var message *chat.Message
 	err = json.Unmarshal(jsonResponse, message)
 	if err != nil {
 		return nil, fmt.Errorf("failed to unserialize response: %w", err)
 	}
 
 	return message, nil
-}
-
-func NewClient(url string, maxTokens uint) *Client {
-	return &Client{URL: url, MaxTokens: maxTokens}
 }
