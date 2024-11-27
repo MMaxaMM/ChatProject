83f6837dae7eebfc6174064e2cd9b6c3e57de9c3
deniol54 <daniil.paholkov@yandex.ru>
2024-11-27 10:50:24 +0300

    Добавлена иконка удаления чата

--- ChatFrontend/src/components/ui/chat-list-item/chatListItem.tsx
+++ ChatFrontend/src/components/ui/chat-list-item/chatListItem.tsx
@@ -2,9 +2,13 @@ import { FC } from 'react';
 import { Link, useMatch } from 'react-router-dom';
 import { TChatListItemUIProps } from './type';
 import styles from './chatListItem.module.css';
+import chatLogo from '../../../images/chatLogo.svg';
+import deleteIcon from '../../../images/deleteIcon.svg';
+import { useState } from 'react';
 
 export const ChatListItemUI: FC<TChatListItemUIProps> = ({ chat, onClick }) => {
   const { content, chat_id, chat_type } = chat;
+  const [isHovered, setIsHovered] = useState(false);
   return (
     <li
       className={`${useMatch(`/chat/${chat_id}`) && styles.chat_list_item_active} ${styles.chat_list_item}`}
@@ -13,6 +17,16 @@ export const ChatListItemUI: FC<TChatListItemUIProps> = ({ chat, onClick }) => {
       <Link to={`/chat/${chat_id}`} className={styles.chat_list_item__title}>
         <span>{content}</span>
       </Link>
+
+      {useMatch(`/chat/${chat_id}`) && (
+        <button
+          className={styles.delete_button}
+          onMouseOver={() => setIsHovered(true)}
+          onMouseOut={() => setIsHovered(false)}
+        >
+          <img src={!isHovered ? chatLogo : deleteIcon} />
+        </button>
+      )}
     </li>
   );
 };
